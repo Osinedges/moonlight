ac5a58bfb163ab494eb6e0f7780a8ccc68784272
Ciaran Reilly <osinedges@gmail.com>
2019-03-15 13:10:39 +0000

    All new animations added, atlas's all  configured correctly. Animations not fully implemented yet.

--- core/src/ciaran/moonlight/LootTable.java
+++ core/src/ciaran/moonlight/LootTable.java
@@ -1,5 +1,25 @@
 package ciaran.moonlight;
 
-public class LootTable {
+import java.util.Random;
 
+public class LootTable {
+  Random rand = new Random();
+  int randomNumber = rand.nextInt(100);
+
+
+
+  public int LootRoll(){
+    if (randomNumber >= 95){
+      return 1;
+    }
+    else if (randomNumber >= 75){
+      return 2;
+    }
+    else if (randomNumber >= 30){
+      return 4;
+    }
+    else{
+      return 5;
+    }
+  }
 }
